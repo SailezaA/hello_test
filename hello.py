11529035f5e87091861bc428192e7bbbf17742b5
Pragya <pragyaawasthi248@gmail.com>
2024-05-12 19:24:56 +0545

    Some edits

---
+++ hello.py
@@ -0,0 +1,13 @@
+# Function to convert list to tuple
+def list_to_tuple(input_list):
+    return tuple(input_list)
+
+# Example list
+my_list = [1, 2, 3, 4, 5]
+
+# Convert list to tuple
+my_tuple = list_to_tuple(my_list)
+
+# Print the tuple
+print("Original List:", my_list)
+print("Tuple:", my_tuple)
